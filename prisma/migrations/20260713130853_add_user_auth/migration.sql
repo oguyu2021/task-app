f94698110cc1949a92be8bbc2ea672e42c0213e6
ogura.yusuke <ogura.yusuke@spiral-platform.co.jp>
2026-07-13 22:18:19 +0900

    feat: 認証機能（api-test.httpまで完了）

---
+++ prisma/migrations/20260713130853_add_user_auth/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Task` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "dueDate" DATETIME NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("completed", "createdAt", "dueDate", "id", "title") SELECT "completed", "createdAt", "dueDate", "id", "title" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
