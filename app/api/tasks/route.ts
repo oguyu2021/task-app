918d8b17c64804aa85cd1e59100bc49918640564
ogura.yusuke <ogura.yusuke@spiral-platform.co.jp>
2026-07-14 23:15:06 +0900

    feat: 認証機能（タスク機能の分離（GET,POST））

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,23 +2,74 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { taskSchema } from "@/schemas/task";
 import { z } from "zod";
+import { auth } from "@/auth";
+
 
 export async function GET() {
+
+  const session = await auth();
+
+
+  if (!session?.user?.id) {
+
+    return NextResponse.json(
+      {
+        message: "認証されていません",
+      },
+      {
+        status: 401,
+      }
+    );
+
+  }
+
+
   const tasks = await prisma.task.findMany({
+
+    where: {
+      userId: Number(session.user.id),
+    },
+
     orderBy: {
       createdAt: "desc",
     },
+
   });
 
+
   return NextResponse.json(tasks);
+
 }
 
+
+
 export async function POST(request: Request) {
+
+  const session = await auth();
+
+
+  if (!session?.user?.id) {
+
+    return NextResponse.json(
+      {
+        message: "認証されていません",
+      },
+      {
+        status: 401,
+      }
+    );
+
+  }
+
+
   const body = await request.json();
+
 
   const result = taskSchema.safeParse(body);
 
-  if(!result.success){
+
+  if (!result.success) {
+
     return NextResponse.json(
       {
         errors: z.flattenError(result.error),
@@ -26,16 +77,35 @@ export async function POST(request: Request) {
       {
         status: 400,
       }
-    )
+    );
+
   }
 
+
   const task = await prisma.task.create({
+
     data: {
+
       title: result.data.title,
-      dueDate: new Date(result.data.dueDate),
+
+      dueDate:
+        new Date(result.data.dueDate),
+
       completed: false,
+
+      userId:
+        Number(session.user.id),
+
     },
+
   });
 
-  return NextResponse.json(task, { status: 201})
+
+  return NextResponse.json(
+    task,
+    {
+      status: 201,
+    }
+  );
+
 }
--- auth.ts
+++ auth.ts
@@ -85,4 +85,27 @@ export const {
 
   },
 
+  callbacks: {
+
+    async jwt({ token, user }) {
+
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+
+
+    async session({ session, token }) {
+
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+
+  },
+
 });
