c4a0119a82243e2861b56db344819baee0d51e76
ogura.yusuke <ogura.yusuke@spiral-platform.co.jp>
2026-07-16 15:09:17 +0900

    feat: タスク期限切れ判定→色分け

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,5 +1,19 @@
 import { Task } from "@/types/task";
 
+//タスク判定関数
+function isOverdue(dueDate: string, completed: boolean) {
+	if (completed) {
+		return false;
+	}
+
+	const today = new Date();
+	today.setHours(0, 0, 0, 0);
+
+	const due = new Date(dueDate);
+
+	return due < today;
+}
+
 type TaskCardProps = {
 	task: Task;
 	onDelete: (id: number) => void;
@@ -14,7 +28,24 @@ export default function TaskCard({
 	onToggle,
 }: TaskCardProps) {
 	return (
-		<div className="flex items-center justify-between rounded-lg border bg-white p-4 shadow-sm">
+		<div
+			className={`
+				flex
+				items-center
+				justify-between
+				rounded-lg
+				border
+				p-4
+				shadow-sm
+				${
+					isOverdue(task.dueDate, task.completed)
+						? "border-red-400 bg-red-50"
+						: task.completed
+						? "bg-gray-100"
+						: "bg-white"
+				}
+			`}
+		>
 				<div className="flex items-center gap-3"></div>
 				
 				<input
@@ -35,6 +66,13 @@ export default function TaskCard({
 				<p className="text-sm text-gray-500">
 					期限:{task.dueDate}
 				</p>
+				{
+					isOverdue(task.dueDate, task.completed) && (
+						<p className="mt-1 text-sm font-bold text-red-600">
+							期限切れ
+						</p>
+					)
+				}
 			</div>
 
 			<div className="flex gap-2">
